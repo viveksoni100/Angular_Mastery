74bbaf91355abdafef152ea2ec17a9143d53cddb
viveksoni100 <viveksoni100@gmail.com>
2021-07-16 16:34:01 +0530

    deep dive made into component

---
+++ Tutor's Work/1.1 cmp-databinding-start.zip/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,36 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  @Output('srCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  /*onAddServer() {
+    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  }*/
+  onAddServer(nameInput, bpInput) {
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: bpInput.value});
+  }
+
+  /*onAddBlueprint() {
+    this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  }*/
+
+  onAddBlueprint(nameInput, bpInput) {
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: bpInput.value});
+  }
+
+}
---
+++ Tutor's Work/1.1 cmp-databinding-start.zip/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // none, native
+})
+export class ServerElementComponent implements OnInit {
+
+  @Input('servElement') element: {type: string, name: string, content: string};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
