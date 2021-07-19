da29313f7c8d4d5fc7b1c6325495b7d8ce0b9dd5
viveksoni100 <viveksoni100@gmail.com>
2021-07-19 18:05:14 +0530

    routing learning is 50% complete

---
+++ Tutor's Work/2.1 routing-start.zip/routing-start/src/app/users/user/user.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {id: number, name: string};
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+
+    /*if not in the same component, use this approach*/
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+            this.user.name = params['name'];
+        }
+      );
+  }
+
+}
