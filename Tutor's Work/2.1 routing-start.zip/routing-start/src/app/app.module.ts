68b34c4c3075dc5a415013ce8895c0781a9b59bb
viveksoni100 <viveksoni100@gmail.com>
2021-07-20 16:55:34 +0530

    .

--- Tutor's Work/2.1 routing-start.zip/routing-start/src/app/app.module.ts
+++ Tutor's Work/2.1 routing-start.zip/routing-start/src/app/app.module.ts
@@ -13,11 +13,12 @@ import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
 import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AuthGuard} from './auth-guard.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {
-    path: 'servers', component: ServersComponent, children: [
+    path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent}
     ]
--- Tutor's Work/31.1 routing-final.zip/routing-final/src/app/home/home.component.ts
+++ Tutor's Work/31.1 routing-final.zip/routing-final/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthService} from '../auth.service';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -14,15 +15,16 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers(id: number) {
-    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: 1}, fragment: 'loading'});
-  }
-
-  onLogout() {
-    this.authService.logout();
+  onLoadServer(id: number) {
+    // complex calculation
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 
   onLogin() {
     this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
